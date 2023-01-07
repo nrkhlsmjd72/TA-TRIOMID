e85b70d130afece31c611e48c81335ccf9c09dfe
nrkhlsmjd72 <nrkhlsmjd72@gmail.com>
2023-01-07 23:14:35 +0700

    Kode ini merupakan sebuah kelas PHP yang menangani pembuatan dan validasi token menggunakan JWT (JSON Web Token). Kelas ini meng-extend kelas Server yang terdapat di direktori "libraries" dan memanfaatkan library JWT yang juga telah di-require di bagian atas kode.

---
+++ Server/application/controllers/Token.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . "libraries/Server.php";
+
+require APPPATH . 'libraries/JWT.php';
+require APPPATH . 'libraries/ExpiredException.php';
+require APPPATH . 'libraries/BeforeValidException.php';
+require APPPATH . 'libraries/SignatureInvalidException.php';
+require APPPATH . 'libraries/JWK.php';
+
+use \Firebase\JWT\JWT;
+
+class Token extends Server
+{
+    function configToken()
+    {
+        $config['exp'] = 36000; //detik
+        $config['key'] = 'key-jwt';
+        return $config;
+    }
+
+    public function authtoken(){        
+        $authHeader = $this->input->request_headers()['Authorization'];  
+        $arr = explode(" ", $authHeader); 
+        $token = $arr[1];        
+        if ($token){
+            try{
+                $decoded = JWT::decode($token, $this->configToken()['key'], array('HS256'));          
+                if ($decoded){
+                    return 1;
+                }
+            } catch (\Exception $e) {                
+                return 0;
+                
+            }
+        }       
+    }
+
+
+    //buat fungsi "POST"
+    function service_post()
+    {
+        date_default_timezone_set("Asia/Jakarta");        
+        $exp = $this->configToken()['exp']+time();
+        $token = array(           
+            "exp" => $exp,                          
+        );
+
+        $jwt = JWT::encode($token, $this->configToken()['key'],'HS256');        
+        $data = array('token' => $jwt, 'exp' => date("d/m/Y H:i",$exp));
+        $this->response($data, 200);
+    }    
+}
